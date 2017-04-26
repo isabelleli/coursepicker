a66c5301ea3239b4f6cd7b5cf9a6b7b73b28cc85
jwang142857 <jenniferwang142857@gmail.com>
2017-04-26 14:33:41 -0400

    let me sync
    pls

--- Course.java
+++ Course.java
@@ -1,4 +1,68 @@
 /* Course.java
+<<<<<<< HEAD
+ * Jennifer Wang, Isabelle Li, Shan Lu
+ * CS 230 Final Project
+ * Object to store information about an individual course
+ */
+
+public class Course{
+  //instance variables
+  private String crn;
+  private String day;
+  private String startTime;
+  private String endTime;
+  private String professor;
+  private String subject;
+  private String session;
+  private String title;
+    
+  public Course(String c, CourseInformation info) {
+    crn = c;
+    String[] infoArray = info.searchDetails(c); //questionable? may need to copy entire array instead of just reading off
+    day = infoArray[0];
+    endTime = infoArray[1]; 
+    professor = infoArray[2];
+    session = infoArray[3];
+    startTime = infoArray[4];
+    subject = infoArray[5];
+    title = inforArray[6];
+
+  }
+  
+  public String getCRN{
+    return crn;
+  }
+  
+  pubic String getDay{
+    return day;
+  }
+  
+  pubic String getStartTime{
+    return startTime;
+  }
+  
+  pubic String getEndTime{
+    return endTime;
+  }
+  
+  pubic String getProf{
+    return professor;
+  }
+  
+  pubic String getSession{
+    return session;
+  }
+  
+  pubic String getTitle{
+    return title;
+  }
+  
+  pubic String getSubject{
+    return subject;
+  }
+
+
+=======
  * CS 230 final project
  * Modified by: ili
  * Modified date: 04/26/17
@@ -7,4 +71,5 @@
 public class Course {
   
   public Course(int CRN)
+>>>>>>> origin/master
 }
