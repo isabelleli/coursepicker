0c35a7fafe72eba875ae38299acf81379d96f77c
jwang142857 <jenniferwang142857@gmail.com>
2017-05-06 16:18:07 -0400

    tester gui should work

--- SearchTest.java
+++ SearchTest.java
@@ -9,7 +9,7 @@ import java.util.*;
 public class SearchTest {
   private LinkedList<Course> searchResults;
   public SearchTest() {
-    String[] c = {"MTH", "09:40AM", "Eni", "01", "08:30AM","CS 111"};
+    String[] c = {"CS 111", "Intro to CS", "01", "Eni", "MWT - 9:50AM - 11:00AM"};
     //String[] d = {"MWTHF", "11:00AM", "Susan", "01", "09:50AM","CHIN", "101"};
     Course a = new Course("56789", c);
     //Course b = new Course("12345", d);
